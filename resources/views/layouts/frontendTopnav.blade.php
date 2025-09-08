231617aa644dbca62acd8994c09ccabf4504752c
salav thapa <salav.thapa@gmail.com>
2025-09-08 12:38:24 +0545

    made some changes on footer & top header

--- resources/views/layouts/frontendTopnav.blade.php
+++ resources/views/layouts/frontendTopnav.blade.php
@@ -1,28 +1,38 @@
 <!-- Top Header -->
 <div class="bg-[#2A143C] text-white text-sm">
-    <div class="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center px-6 py-2 md:space-y-0">
+    <div class="max-w-7xl mx-auto flex justify-between items-center px-6 py-2">
+
         <!-- Left: Email & Phone -->
-        <div class="flex flex-col md:flex-row md:space-x-4 space-y-1 md:space-y-0">
-            <span class="flex items-center justify-center md:justify-start font-heading font-medium">
-                <i class="fas fa-envelope mr-2 text-yellow-300 text-lg"></i>
-                pixelprintnepal@gmail.com
-            </span>
-            <span class="flex items-center justify-center md:justify-start font-heading font-medium">
-                <i class="fa-solid fa-phone mr-2 text-yellow-500 text-lg"></i>
-                01-5914668 | 9801433001
-            </span>
+        <div class="flex items-center space-x-4">
+            <a href="#" class="flex items-center font-heading font-medium">
+                <i class="fas fa-envelope text-yellow-300 text-lg"></i>
+                <span class="hidden md:inline ml-2">pixelprintnepal@gmail.com</span>
+            </a>
+            <a href="#" class="flex items-center font-heading font-medium">
+                <i class="fa-solid fa-phone text-yellow-500 text-lg"></i>
+                <span class="hidden md:inline ml-2">01-5914668 | 9801433001</span>
+            </a>
         </div>
 
         <!-- Right: Social Icons -->
-        <div class="flex items-center space-x-2 mt-2 md:mt-0">
-            <a href="#" class="p-1 border rounded-full hover:bg-yellow-500 hover:text-black m-0 w-[29px] text-center"><i
-                    class="fa-brands fa-facebook"></i></a>
-            <a href="#" class="p-1 border rounded-full hover:bg-yellow-500 hover:text-black w-[29px] text-center"><i
-                    class="fab fa-instagram"></i></a>
-            <a href="#" class="p-1 border rounded-full hover:bg-yellow-500 hover:text-black w-[29px] text-center"><i
-                    class="fab fa-x-twitter"></i></a>
-            <a href="#" class="p-1 border rounded-full hover:bg-yellow-500 hover:text-black w-[29px] text-center"><i
-                    class="fab fa-whatsapp"></i></a>
+        <div class="flex items-center space-x-3">
+            <a href="#"
+                class="w-8 h-8 flex items-center justify-center border rounded-full hover:bg-yellow-500 hover:text-black">
+                <i class="fa-brands fa-facebook"></i>
+            </a>
+            <a href="#"
+                class="w-8 h-8 flex items-center justify-center border rounded-full hover:bg-yellow-500 hover:text-black">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="#"
+                class="w-8 h-8 flex items-center justify-center border rounded-full hover:bg-yellow-500 hover:text-black">
+                <i class="fab fa-x-twitter"></i>
+            </a>
+            <a href="#"
+                class="w-8 h-8 flex items-center justify-center border rounded-full hover:bg-yellow-500 hover:text-black">
+                <i class="fab fa-whatsapp"></i>
+            </a>
         </div>
+
     </div>
 </div>
